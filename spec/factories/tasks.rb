9be2343feff43875264a41fc7c3d996f11556643
kimurasayo <otaemiiin@gmail.com>
2021-03-29 13:36:10 +0900

    Fix: traitの名称をわかりやすいdoneに変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     deadline { Random.rand(from..to) }
     association :project
 
-    trait :add do
+    trait :done do
       status { :done }
       completion_date { Time.current.yesterday }
     end
